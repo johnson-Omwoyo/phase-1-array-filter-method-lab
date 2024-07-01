ce472c551a6f4850e2ab0c0fb806a22e3f219e47
Johnson Omwoyo <johnsonomwoyo100@gmail.com>
2024-07-01 18:17:29 +0300

    filter works nicely

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+let drivers = [
+  { name: "koisan", age: 32 },
+  { name: "kaisa", age: 29 },
+  { name: "kaisa", age: 39 },
+];
+let findMatching = (arr, str) => {
+  return arr.filter((arrr) => arrr.toLowerCase() === str.toLowerCase());
+};
+function fuzzyMatch(arr, str) {
+  return arr.filter(
+    (arrr) => arrr.toLowerCase().indexOf(str.toLowerCase()) === 0
+  );
+}
+
+function matchName(arr, str) {
+  return arr.filter((record) => record.name === str);
+}
